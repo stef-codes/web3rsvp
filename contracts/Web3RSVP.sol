b7d175906f4d9823144657c70364d60fc5ada01a
Stefanie <stefcodes@gmail.com>
2022-08-04 20:30:23 -0700

    create struct and add properties

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -3,20 +3,16 @@ pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
 
-contract Greeter {
-    string private greeting;
-
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
-    }
-
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
-
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+contract Web3RSVP {
+    struct CreateEvent{
+        bytes32 eventId;
+        string eventDataCID; 
+        address eventOwner; 
+        uint256 eventTimestamp; 
+        uint256 deposit; 
+        uint256 maxCapacity; 
+        address[] confirmedRSVPs;
+        address[] claimedRSVPs; 
+        bool paidOut; 
     }
 }
