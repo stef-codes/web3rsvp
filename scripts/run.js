bbf495023d316091dc1e3297f2dba9a9bd815b2a
Stefanie <stefcodes@gmail.com>
2022-08-12 17:20:47 -0700

    add hardhat deployment

--- scripts/run.js
+++ scripts/run.js
@@ -1,32 +1,32 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const hre = require("hardhat"); 
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+const main = async () => {
+  // use hardhat to deploy the contract locally 
+  const rsvpContractFactory = await hre.ethers.getContractFactory("Web3RSVP");
+  const rsvpContract = await rsvpContractFactory.deploy();
+  await rsvpContract.deployed();
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
 
-  await greeter.deployed();
+  console.log("Contract deployed to:", rsvpContract.address);
 
-  console.log("Greeter deployed to:", greeter.address);
-}
+  const [deployer, address1, address2] = await hre.ethers.getSigners();
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+  let deposit = hre.ethers.utils.parseEther("1");
+  let maxCapacity = 3;
+  let timestamp = 1718926200;
+  let eventDataCID =
+    "bafybeibhwfzx6oo5rymsxmkdxpmkfwyvbjrrwcl7cekmbzlupmp5ypkyfi";
+}; 
+
+const runMain = async () => {
+  try {
+    await main(); 
+    Process.exit(0); 
+  } catch (error) {
+    console.log(error); 
+    process.exit(1); 
+  }
+
+}; 
+
+runMain(); 
